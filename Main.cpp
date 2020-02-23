0c28da90e6119477e4f9403e15e7c174fe9633e5
Gregory Hapgood <greghap@gmail.com>
2020-02-23 14:21:17 -0600

    Add CMakeLists.txt

---
+++ Main.cpp
@@ -0,0 +1,26 @@
+#include <GLFW/glfw3.h>
+#include <vulkan\vulkan.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+
+    return 0;
+}
